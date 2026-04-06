eb0fa1ff9cf037577c59cc4af7dbd2d8138bba48
AnshiX9058 <anshikavermaanshikaverma48@gmail.com>
2026-04-06 19:33:38 +0530

    Add files via upload

---
+++ NoOfSwap.java
@@ -0,0 +1,30 @@
+package p1;
+import java.util.Scanner;
+public class NoOfSwap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int swapCount = 0;  
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapCount++;
+                }
+            }
+            System.out.println("Pass " + (i + 1) + " - Number of swaps: " + swapCount);
+        }
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
