577f2e6d20e9346d28a53d6e892b7b15f96408ae
AnshiX9058 <anshikavermaanshikaverma48@gmail.com>
2026-04-06 19:33:52 +0530

    Add files via upload

---
+++ NoOfSwap2.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class NoOfSwap2 {
+
+	public NoOfSwap2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
