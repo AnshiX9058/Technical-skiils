278bdacbd20f94f8ee9c876b4c22019bc55bdfc4
AnshiX9058 <anshikavermaanshikaverma48@gmail.com>
2026-04-09 12:47:57 +0530

    Add files via upload

---
+++ TraversalLinkedList.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class TraversalLinkedList {
+
+	public TraversalLinkedList() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
